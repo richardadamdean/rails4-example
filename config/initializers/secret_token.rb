ba244be753f189be933fa7130c1ef227b4a6e205
Richard Dean <rdean@dius.com.au>
2013-07-05 10:38:29 +1000

    Generate skeleton rails app and create a single db table to test chef deploys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Example::Application.config.secret_key_base = '14cc28e7f4882a7e6f253acf426e2f4b706e730a515b75715cc20c74cd3c80a211673c78d5990d7f7382e9cd3677cfccc3218822827b6484fc04e34a895caff1'
